3a43ae37ec58ea920e062ae8b0d984109e20fd56
cyonits <cyonitss@gmail.com>
2023-08-07 14:13:43 +0900

    create spring-project

---
+++ src/test/java/com/codestates/testrepository/TestRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.testrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
